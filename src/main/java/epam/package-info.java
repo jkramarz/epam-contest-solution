7c950f3c1f13712588bcb182d8c3d3756de7054f
Łukasz Dubiel <lukasz.dubiel@llp.pl>
2012-11-27 09:12:22 +0100

    Finish moving to maven.

--- src/main/java/epam/package-info.java
+++ src/main/java/epam/package-info.java
@@ -1,5 +1,4 @@
 /**
- * @author £ukasz Dubiel <bambucha14 at gmail dot com>, Jakub Kramarz <lenwe at lenwe dot com>
+ * @author ≈Åukasz Dubiel <bambucha14 at gmail dot com>, Jakub Kramarz <lenwe at lenwe dot com>
  * Solution of the problem from <epam> Java Runs Really Fast contest at 26/11/2012 in Cracow
  */
-package epam;
